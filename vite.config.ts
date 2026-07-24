057360e334804cb5f98e14e2e472e137e9a41b1e
apolinariolizete5-afk <apolinariolizete5@gmail.com>
2026-07-24 10:15:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+    // Redirect TanStack Start's bundled server entry to src/server.ts
+    // nitro/vite builds from this
     server: { entry: "server" },
   },
   server: {
-    allowedHosts: true,
+    allowedHosts: ["mozaempregos.onrender.com"],
   },
 });
-
